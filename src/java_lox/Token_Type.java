a2c0e10a2ff62a362e64af24e2a45b93966a4347
microsoftjava <121544303+microsoftjava@users.noreply.github.com>
2022-12-31 21:50:20 +0800

    made insignificant changes to the comments in Token_Type.java

--- src/java_lox/Token_Type.java
+++ src/java_lox/Token_Type.java
@@ -1,20 +1,20 @@
 package java_lox;
 
 enum Token_Type {
-    //* Single-character tokens. */
+    //* Single-character tokens.
     LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
     COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
 
-    //* One- or two-character tokens. */
+    //* One- or two-character tokens.
     BANG, BANG_EQUAL,
     EQUAL, EQUAL_EQUAL,
     GREATER, GREATER_EQUAL,
     LESS, LESS_EQUAL,
 
-    //* Literals. */
+    //* Literals.
     IDENTIFIER, STRING, NUMBER,
 
-    //* Keywords. */
+    //* Keywords.
     AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR,
     PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE,
 
