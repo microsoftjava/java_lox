00f1fc3c0c4cea052b9c91583052714585fd6ed5
microsoftjava <121544303+microsoftjava@users.noreply.github.com>
2022-12-31 18:49:59 +0800

    completed the basic framework for the interpreter

--- src/java_lox/Java_Lox.java
+++ src/java_lox/Java_Lox.java
@@ -7,9 +7,36 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.Scanner;
 
 public class Java_Lox {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+    public static void main(String[] args) throws IOException {
+        if (args.length > 1) {
+            System.out.println("Usage: java_lox [script]");
+            System.exit(64);}
+        else if (args.length == 1) {
+            runFile(args[0]);}
+        else {
+            runPrompt();}}
+
+    private static void runFile(String path) throws IOException {
+        byte[] bytes = Files.readAllBytes(Paths.get(path));
+        run(new String(bytes, Charset.defaultCharset()));}
+    
+    private static void runPrompt() throws IOException {
+        InputStreamReader input = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(input);
+
+        for (;;) {
+            System.out.println("> ");
+            String line = reader.readLine();
+            if (line == null) break;
+            run(line);}}
+    
+    private static void run(String source) {
+        Scanner scanner = new Scanner(source);
+        List<Token> tokens = scanner.scanTokens();
+
+        for (Token token : tokens) {
+            System.out.println(token);}}
 }
