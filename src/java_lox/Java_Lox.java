fa240d8ec19ab6238d3df8b8358262f534542bdb
microsoftjava <121544303+microsoftjava@users.noreply.github.com>
2022-12-31 16:51:51 +0800

    i got to the part where he shows the interpreter framework

---
+++ src/java_lox/Java_Lox.java
@@ -0,0 +1,15 @@
+package java_lox;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Java_Lox {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
