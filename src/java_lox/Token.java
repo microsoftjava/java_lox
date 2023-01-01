fc160d79100884e5858ca0e08e8c6bda168c8e99
microsoftjava <121544303+microsoftjava@users.noreply.github.com>
2023-01-01 11:49:37 +0800

    added Token() constructor and toString() method to Token.java

--- src/java_lox/Token.java
+++ src/java_lox/Token.java
@@ -1,5 +1,17 @@
 package java_lox;
 
 public class Token {
+    final Token_Type type;
+    final String lexeme;
+    final Object literal;
+    final int line;
+
+    Token(Token_Type type, String lexeme, Object literal, int line) {
+        this.type = type;
+        this.lexeme = lexeme;
+        this.literal = literal;
+        this.line = line;}
     
+    public String toString() {
+        return type + " " + lexeme + " " + literal;}
 }
