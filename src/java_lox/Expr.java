321f949cc7d4b515959abd4bf60e4544b807dc68
microsoftjava <121544303+microsoftjava@users.noreply.github.com>
2023-01-02 19:02:09 +0800

    added Grouping, Literal and Unary subclasses to Expr abstract class

--- src/java_lox/Expr.java
+++ src/java_lox/Expr.java
@@ -5,13 +5,34 @@ abstract class Expr {
     //gist this is for binary expressions
     static class Binary extends Expr {
         //gist this is the constructor for binary expressions
-        Binary(Expr left, Token operator, Expr right) {
-            this.left = left;
-            this.operator = operator;
-            this.right = right;}
+        Binary(Expr left, Token operator, Expr right)
+        {this.left = left;this.operator = operator;this.right = right;}
         
-        final Expr left;
-        final Token operator;
-        final Expr right;
+        final Expr left;final Token operator;final Expr right;
+    }
+
+    //gist this is for groupings
+    static class Grouping extends Expr {
+        //gist this is the constructor for groupings
+        Grouping(Expr expression) {this.expression = expression;}
+
+        final Expr expression;
+    }
+
+    //gist this is for literals
+    static class Literal extends Expr {
+        //gist this is the constructor for literals
+        Literal(Object value) {this.value = value;}
+
+        final Object value;
+    }
+
+    //gist this is for unary expressions
+    static class Unary extends Expr {
+        //gist this is the constructor for unary expressions
+        Unary(Token operator, Expr right)
+        {this.operator = operator;this.right = right;}
+        
+        final Token operator;final Expr right;
     }
 }
