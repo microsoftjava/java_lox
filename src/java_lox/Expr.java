f433e44ec5c15385476d1be997d64cc25a393f62
microsoftjava <121544303+microsoftjava@users.noreply.github.com>
2023-01-02 21:04:39 +0800

    implemented visitor pattern for Expr abstract class

--- src/java_lox/Expr.java
+++ src/java_lox/Expr.java
@@ -2,6 +2,17 @@ package java_lox;
 
 //gist this is for expressions
 abstract class Expr {
+    //gist this is for interactions between expressions
+    interface Visitor<R> {
+        R visitBinaryExpr(Binary expr);
+        R visitGroupingExpr(Grouping expr);
+        R visitLiteralExpr(Literal expr);
+        R visitUnaryExpr(Unary expr);
+    }
+
+    //gist this is also for interactions between expressions
+    abstract <R> R accept(Visitor<R> visitor);
+
     //gist this is for binary expressions
     static class Binary extends Expr {
         //gist this is the constructor for binary expressions
@@ -9,6 +20,10 @@ abstract class Expr {
         {this.left = left;this.operator = operator;this.right = right;}
         
         final Expr left;final Token operator;final Expr right;
+
+        @Override
+        <R> R accept(Visitor<R> visitor)
+        {return visitor.visitBinaryExpr(this);}
     }
 
     //gist this is for groupings
@@ -17,6 +32,10 @@ abstract class Expr {
         Grouping(Expr expression) {this.expression = expression;}
 
         final Expr expression;
+
+        @Override
+        <R> R accept(Visitor<R> visitor)
+        {return visitor.visitGroupingExpr(this);}
     }
 
     //gist this is for literals
@@ -25,6 +44,10 @@ abstract class Expr {
         Literal(Object value) {this.value = value;}
 
         final Object value;
+
+        @Override
+        <R> R accept(Visitor<R> visitor)
+        {return visitor.visitLiteralExpr(this);}
     }
 
     //gist this is for unary expressions
@@ -34,5 +57,9 @@ abstract class Expr {
         {this.operator = operator;this.right = right;}
         
         final Token operator;final Expr right;
+
+        @Override
+        <R> R accept(Visitor<R> visitor)
+        {return visitor.visitUnaryExpr(this);}
     }
 }
