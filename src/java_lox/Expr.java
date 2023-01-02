0348dba32ac26af805240f44900f45e1ed834276
microsoftjava <121544303+microsoftjava@users.noreply.github.com>
2023-01-02 15:02:33 +0800

    added Binary subclass to Expr abstract class

--- src/java_lox/Expr.java
+++ src/java_lox/Expr.java
@@ -1,5 +1,17 @@
 package java_lox;
 
+//gist this is for expressions
 abstract class Expr {
-    
+    //gist this is for binary expressions
+    static class Binary extends Expr {
+        //gist this is the constructor for binary expressions
+        Binary(Expr left, Token operator, Expr right) {
+            this.left = left;
+            this.operator = operator;
+            this.right = right;}
+        
+        final Expr left;
+        final Token operator;
+        final Expr right;
+    }
 }
