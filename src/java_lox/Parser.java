04b10f0feb1962ba52b2bfe4134474f333ff4f49
microsoftjava <121544303+microsoftjava@users.noreply.github.com>
2023-01-02 22:44:21 +0800

    added some methods to Parser class

--- src/java_lox/Parser.java
+++ src/java_lox/Parser.java
@@ -13,4 +13,34 @@ public class Parser {
     Parser(List<Token> tokens) {
         this.tokens = tokens;
     }
+
+    //gist this is for expressions
+    private Expr expression() {
+        return equality();
+    }
+
+    //gist this evaluates expressions that contain != or ==
+    private Expr equality() {
+        Expr expr = comparison();
+
+        while (match(BANG_EQUAL, EQUAL_EQUAL)) {
+            Token operator = previous();
+            Expr right = comparison();
+            expr = new Expr.Binary(expr, operator, right);
+        }
+
+        return expr;
+    }
+
+    //gist this compares the current token with the provided tokens
+    private boolean match(Token_Type... types) {
+        for (Token_Type type : types) {
+            if (check(type)) {
+                advance();
+                return true;
+            }
+        }
+        
+        return false;
+    }
 }
