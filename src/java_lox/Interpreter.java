aff297cb47372b798b5d76c46089f5f71f229a6f
microsoftjava <121544303+microsoftjava@users.noreply.github.com>
2023-01-03 18:22:06 +0800

    did stuff i am malding because the parser only tells me about the EOF that the scanner placed at the end of the list of tokens

---
+++ src/java_lox/Interpreter.java
@@ -0,0 +1,37 @@
+package java_lox;
+
+public class Interpreter implements Expr.Visitor<Object> {
+    private Object evaluate(Expr expr) {
+        return expr.accept(this);
+    }
+
+    @Override
+    public Object visitGroupingExpr(Expr.Grouping expr) {
+        return evaluate(expr.expression);
+    }
+
+    @Override
+    public Object visitLiteralExpr(Expr.Literal expr) {
+        return expr.value;
+    }
+
+    @Override
+    public Object visitUnaryExpr(Expr.Unary expr) {
+        Object right = evaluate(expr.right);
+
+        switch (expr.operator.type) {
+            case BANG:
+                return !isTruthy(right);
+            case MINUS:
+                return -(double)right;
+        }
+
+        return null;
+    }
+
+    private boolean isTruthy(Object object) {
+        if (object == null) return false;
+        if (object instanceof Boolean) return (boolean)object;
+        return true;
+    }
+}
