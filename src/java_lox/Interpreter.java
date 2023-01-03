7769b9ae4937819c8fd4320a7d2df1098a888f8e
microsoftjava <121544303+microsoftjava@users.noreply.github.com>
2023-01-03 18:32:55 +0800

    i am still malding

--- src/java_lox/Interpreter.java
+++ src/java_lox/Interpreter.java
@@ -6,6 +6,43 @@ public class Interpreter implements Expr.Visitor<Object> {
     }
 
     @Override
+    public Object visitBinaryExpr(Expr.Binary expr) {
+        Object left = evaluate(expr.left);
+        Object right = evaluate(expr.right); 
+
+        switch (expr.operator.type) {
+            case BANG_EQUAL: return !isEqual(left, right);
+            case EQUAL_EQUAL: return isEqual(left, right);
+            case GREATER:
+                return (double)left > (double)right;
+            case GREATER_EQUAL:
+                return (double)left >= (double)right;
+            case LESS:
+                return (double)left < (double)right;
+            case LESS_EQUAL:
+                return (double)left <= (double)right;
+            case MINUS:
+                return (double)left - (double)right;
+            case PLUS:
+                if (left instanceof Double && right instanceof Double) {
+                  return (double)left + (double)right;
+                } 
+        
+                if (left instanceof String && right instanceof String) {
+                  return (String)left + (String)right;
+                }
+        
+                break;
+            case SLASH:
+                return (double)left / (double)right;
+            case STAR:
+                return (double)left * (double)right;
+        }
+
+        return null;
+    }
+
+    @Override
     public Object visitGroupingExpr(Expr.Grouping expr) {
         return evaluate(expr.expression);
     }
@@ -23,15 +60,28 @@ public class Interpreter implements Expr.Visitor<Object> {
             case BANG:
                 return !isTruthy(right);
             case MINUS:
+                checkNumberOperand(expr.operator, right);
                 return -(double)right;
         }
 
         return null;
     }
 
+    private void checkNumberOperand(Token operator, Object operand) {
+        if (operand instanceof Double) return;
+        throw new RuntimeError(operator, "Operand must be a number.");
+    }
+
     private boolean isTruthy(Object object) {
         if (object == null) return false;
         if (object instanceof Boolean) return (boolean)object;
         return true;
+    }
+
+    private boolean isEqual(Object a, Object b) {
+        if (a == null && b == null) return true;
+        if (a == null) return false;
+    
+        return a.equals(b);
     }
 }
