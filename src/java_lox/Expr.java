d1673c17d6829a997aa6ee56b6e158f957625cfe
microsoftjava <121544303+microsoftjava@users.noreply.github.com>
2023-01-03 22:41:13 +0800

    there is so much code now... i'm going to die when i make my own programming language

--- src/java_lox/Expr.java
+++ src/java_lox/Expr.java
@@ -8,6 +8,7 @@ abstract class Expr {
         R visitBinaryExpr(Binary expr);
         R visitGroupingExpr(Grouping expr);
         R visitLiteralExpr(Literal expr);
+        R visitLogicalExpr(Logical expr);
         R visitUnaryExpr(Unary expr);
         R visitVariableExpr(Variable expr);
     }
@@ -65,6 +66,23 @@ abstract class Expr {
         @Override
         <R> R accept(Visitor<R> visitor)
         {return visitor.visitLiteralExpr(this);}
+    }
+
+    static class Logical extends Expr {
+        Logical(Expr left, Token operator, Expr right) {
+            this.left = left;
+            this.operator = operator;
+            this.right = right;
+        }
+    
+        @Override
+        <R> R accept(Visitor<R> visitor) {
+            return visitor.visitLogicalExpr(this);
+        }
+    
+        final Expr left;
+        final Token operator;
+        final Expr right;
     }
 
     //gist this is for unary expressions
