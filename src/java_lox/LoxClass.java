fb5c1a97710247053f47823984f0faa3beed8e8d
microsoftjava <121544303+microsoftjava@users.noreply.github.com>
2023-01-04 16:06:33 +0800

    created new LoxClass.java file

---
+++ src/java_lox/LoxClass.java
@@ -0,0 +1,28 @@
+package java_lox;
+
+import java.util.List;
+import java.util.Map;
+
+public class LoxClass implements LoxCallable {
+    final String name;
+
+    LoxClass(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    @Override
+    public Object call(Interpreter interpreter, List<Object> arguments) {
+        LoxInstance instance = new LoxInstance(this);
+        return instance;
+    }
+
+    @Override
+    public int arity() {
+        return 0;
+    }
+}
