fc4bbab80716f0aa57fb2011445f3b04a89e679f
microsoftjava <121544303+microsoftjava@users.noreply.github.com>
2023-01-04 12:06:10 +0800

    added or() and and() to Parser

--- src/java_lox/Parser.java
+++ src/java_lox/Parser.java
@@ -102,7 +102,7 @@ public class Parser {
     }
 
     private Expr assignment() {
-        Expr expr = equality();
+        Expr expr = or();
     
         if (match(EQUAL)) {
             Token equals = previous();
@@ -114,6 +114,30 @@ public class Parser {
             }
     
             error(equals, "Invalid assignment target."); 
+        }
+    
+        return expr;
+    }
+
+    private Expr or() {
+        Expr expr = and();
+    
+        while (match(OR)) {
+            Token operator = previous();
+            Expr right = and();
+            expr = new Expr.Logical(expr, operator, right);
+        }
+    
+        return expr;
+    }
+
+    private Expr and() {
+        Expr expr = equality();
+    
+        while (match(AND)) {
+            Token operator = previous();
+            Expr right = equality();
+            expr = new Expr.Logical(expr, operator, right);
         }
     
         return expr;
