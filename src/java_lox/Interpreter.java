b05eea742c5d81ca3a56f7742e7b8f7eb92eca1e
microsoftjava <121544303+microsoftjava@users.noreply.github.com>
2023-01-04 14:51:15 +0800

    cool stuff happened maybe :)

--- src/java_lox/Interpreter.java
+++ src/java_lox/Interpreter.java
@@ -67,7 +67,7 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 
     @Override
     public Void visitFunctionStmt(Stmt.Function stmt) {
-        LoxFunction function = new LoxFunction(stmt);
+        LoxFunction function = new LoxFunction(stmt, environment);
         environment.define(stmt.name.lexeme, function);
         return null;
     }
