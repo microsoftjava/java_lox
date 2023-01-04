0b0b3dab928b4078e6e5556d7712d5e7c8e53c27
microsoftjava <121544303+microsoftjava@users.noreply.github.com>
2023-01-04 16:10:00 +0800

    created new LoxInstance.java file

---
+++ src/java_lox/LoxInstance.java
@@ -0,0 +1,17 @@
+package java_lox;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LoxInstance {
+    private LoxClass klass;
+
+    LoxInstance(LoxClass klass) {
+        this.klass = klass;
+    }
+
+    @Override
+    public String toString() {
+        return klass.name + " instance";
+    }
+}
