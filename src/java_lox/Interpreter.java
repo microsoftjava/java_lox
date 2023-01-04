3875395803a03d3ed95245ea8f240ff3e147a387
microsoftjava <121544303+microsoftjava@users.noreply.github.com>
2023-01-04 12:08:16 +0800

    added logical expressions to Interpreter

--- src/java_lox/Interpreter.java
+++ src/java_lox/Interpreter.java
@@ -139,6 +139,19 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
     }
 
     @Override
+    public Object visitLogicalExpr(Expr.Logical expr) {
+        Object left = evaluate(expr.left);
+
+        if (expr.operator.type == Token_Type.OR) {
+            if (isTruthy(left)) return left;
+        } else {
+            if (!isTruthy(left)) return left;
+        }
+
+        return evaluate(expr.right);
+    }
+
+    @Override
     public Object visitUnaryExpr(Expr.Unary expr) {
         Object right = evaluate(expr.right);
 
