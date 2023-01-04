73bf43b3d23c384004871b44e3357df37324a5d7
microsoftjava <121544303+microsoftjava@users.noreply.github.com>
2023-01-04 13:19:24 +0800

    there is a clock now

--- src/java_lox/Interpreter.java
+++ src/java_lox/Interpreter.java
@@ -4,7 +4,23 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
-    private Environment environment = new Environment();
+    final Environment globals = new Environment();
+    private Environment environment = globals;
+
+    Interpreter() {
+        globals.define("clock", new LoxCallable() {
+            @Override
+            public int arity() { return 0; }
+    
+            @Override
+            public Object call(Interpreter interpreter, List<Object> arguments) {
+                return (double)System.currentTimeMillis() / 1000.0;
+            }
+    
+            @Override
+            public String toString() { return "<native fn>"; }
+        });
+    }
 
     void interpret(List<Stmt> statements) {
         try {
@@ -156,7 +172,7 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
                 function.arity() + " arguments but got " +
                 arguments.size() + ".");
         }
-        
+
         return function.call(this, arguments);
     }
 
